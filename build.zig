ee6d4b1b05eb1e0c85d835a7695770ca6dbbbfab
tiawl <pabtomas@protonmail.com>
2025-03-17 16:15:00 +0100

    typo into build.zig

--- build.zig
+++ build.zig
@@ -284,7 +284,7 @@ pub fn build(builder: *std.Build) !void {
         .toolbox = .{
             .name = "tiawl/toolbox",
             .host = toolbox.Repository.Host.github,
-            .ref = toolbox.Repository.Reference.tag,
+            .ref = toolbox.Repository.Reference.commit,
         },
     }, .{
         .spirv = .{
